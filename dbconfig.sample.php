ada8bd0d4c0428529aefb79d193bf4c51bab38e7
faizzadham <faizadham9@gmail.com>
2026-05-08 11:55:14 +0800

    databaseconfigfix

---
+++ dbconfig.sample.php
@@ -0,0 +1,7 @@
+<?php
+// Copy this file to dbconfig.php and update the values for your environment.
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'gym_db');
